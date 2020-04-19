7f02e3f6ff1a904288a51f3ca96dc1cbb8c89656
Miguel Siordia <miguelsiordia99@gmail.com>
2020-04-18 19:28:39 -0500

    Slightly modified classes like user and champion

--- src/main/java/Betting/Bet.java
+++ src/main/java/Betting/Bet.java
@@ -1,12 +1,14 @@
 package main.java.Betting;
 
+import main.java.User;
+
 public final class Bet {
-private String user;
+private User user;
 private int rep;
 private float percentChange;
 private String champion;
 
-public Bet(String user, int rep, float percentChange, String champion) {
+public Bet(User user, int rep, float percentChange, String champion) {
 	this.user = user;
 	this.rep = rep;
 	this.percentChange = percentChange;
--- src/main/java/Champion.java
+++ src/main/java/Champion.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 public class Champion {
 
+	public String name;
+	public float winRate;
+	public float pickRate;
+	public float banRate;
+	
+	private List<Double> pastWinRate; //Need to be able to show trend data, maybe store old rates?
+	
 	public Champion(List<String> userStrings) {
+		this.name = userStrings.get(0);
+		
 	}
 
 }
