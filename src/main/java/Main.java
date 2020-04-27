0df2bd3e2c5596e5549512a090f9e3e80f5d4bdb
Zerowaltz <xiaochi_zhang@brown.edu>
2020-04-26 20:13:23 -0500

    more spark setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,14 @@
-package main.java;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Map;
-
-import com.google.common.collect.ImmutableMap;
 
 import RiotAPI.RiotAPI;
 import freemarker.template.Configuration;
-import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
-import spark.Spark;
-import spark.TemplateViewRoute;
+import com.google.common.collect.ImmutableMap;
+import spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 public class Main {
 
@@ -22,7 +19,7 @@ public class Main {
 	
 	private static FreeMarkerEngine createEngine() {
 	    Configuration config = new Configuration();
-	    File templates = new File("src/main/resources/spark/template/freemarker");
+	    File templates = new File("src/resources/static");
 	    try {
 	      config.setDirectoryForTemplateLoading(templates);
 	    } catch (IOException ioe) {
@@ -34,25 +31,39 @@ public class Main {
 
 	  private void runSparkServer(int port) {
 	    Spark.port(port);
-	    Spark.externalStaticFileLocation("src/main/resources/static");
+	    Spark.externalStaticFileLocation("src/resources/static");
 	    Spark.exception(Exception.class, new ExceptionPrinter());
 
 	    FreeMarkerEngine freeMarker = createEngine();
 
 	    // Setup Spark Routes
-	    Spark.get("/stars", new FrontHandler(), freeMarker);
-	    Spark.post("/neighbors_name", new NeighborsNameHandler(), freeMarker);
+		  Spark.get("/home", new FrontHandler(), freeMarker);
 	  }
 	  
 	  private static class FrontHandler implements TemplateViewRoute {
 		    @Override
 		    public ModelAndView handle(Request req, Response res) {
-		      Map<String, Object> variables = ImmutableMap.of("title", "Stars", "suggestions", "");
+		      Map<String, Object> variables = ImmutableMap.of("userReputation", "");
 
-		      return new ModelAndView(variables, "query.ftl");
+		      return new ModelAndView(variables, "template.ftl");
 		    }
 		  }
-	
-	
 
+
+	/**
+	 * Display an error page when an exception occurs in the server.
+	 */
+	private static class ExceptionPrinter implements ExceptionHandler {
+		@Override
+		public void handle(Exception e, Request req, Response res) {
+			res.status(500);
+			StringWriter stacktrace = new StringWriter();
+			try (PrintWriter pw = new PrintWriter(stacktrace)) {
+				pw.println("<pre>");
+				e.printStackTrace(pw);
+				pw.println("</pre>");
+			}
+			res.body(stacktrace.toString());
+		}
+	}
 }
