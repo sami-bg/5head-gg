87ed4042ddcb58847c3db6372ecdad53d2bd19af
Zerowaltz <xiaochi_zhang@brown.edu>
2020-05-04 11:23:50 -0500

    use test

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,11 +1,22 @@
-
+import Database.DatabaseEntryFiller;
+import Database.DatabaseHandler;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class UserTest {
+    DatabaseHandler DHTest;
+    DatabaseEntryFiller DEFTest;
+    @Before // setup()
+    public void before() throws Exception {
+         DEFTest = new DatabaseEntryFiller();
+         DHTest = new DatabaseHandler();
+    }
 
     @Test
-    public void MakeUserTest(){
-
+    public void MakeUserTest() throws Exception {
+        before();
+    DHTest.addNewUser("testUser.", "testUser", "10000", "", "");
+       // PreparedStatement prep = conn.prepareStatement("DELETE from Users");
+        
     }
 }
